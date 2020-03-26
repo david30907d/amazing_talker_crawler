59d0df012743c1dfecd2c9f5a643c1069401d8b0
davidtnfsh <davidtnfsh@gmail.com>
2020-03-26 09:37:37 +0800

    feat(crawler): finish skeleton
    get the name, url and price of tutor who is available right now
    
    BREAKING CHANGE: first commit

---
+++ project/main.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+from tenacity import retry, wait_fixed, stop_after_delay, stop_after_attempt
+
+
+class AmazingTalker(object):
+    def __init__(self, lang):
+        self._prefix = "https://tw.amazingtalker.com"
+        self._url = f"{self._prefix}/tutors/{lang}"
+        self._sess = requests.session()
+
+    @property
+    def url_prefix(self):
+        return self._prefix
+
+    @property
+    def url(self):
+        return self._url
+
+    def url_with_page_num(self, page_num):
+        return f"{self.url}?page={page_num}"
+
+    @property
+    def sess(self):
+        return self._sess
+
+    def crawl_every_page(self):
+        for page_num in range(1, 50):
+            if self._crawl_single_page(page_num) == "fail":
+                break
+
+    @retry(
+        stop=(stop_after_delay(5) | stop_after_attempt(3)),
+        wait=wait_fixed(2),
+        retry_error_callback=lambda x: "fail",
+    )
+    def _crawl_single_page(self, page_num):
+        resp = self.sess.get(self.url_with_page_num(page_num))
+        if resp.status_code != 200:
+            raise Exception("fail!")
+        html_doc = resp.text
+        soup = BeautifulSoup(html_doc, "html.parser")
+        self._output(
+            zip(
+                soup.select("li.at-shadow-box div.book"),
+                soup.select("li.at-shadow-box div.name-and-rating"),
+                soup.select("div.price"),
+            )
+        )
+
+    def _output(self, payloads):
+        for booking_status, name_and_rating, price in payloads:
+            if booking_status.text == "預約立即體驗":
+                print(
+                    name_and_rating.text,
+                    f"{self.url_prefix}{name_and_rating.find('a')['href']}",
+                    price.text,
+                )
+
+
+if __name__ == "__main__":
+    a = AmazingTalker(lang="japanese")
+    a.crawl_every_page()
